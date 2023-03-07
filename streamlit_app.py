864369d152aea446a0079e7e6720e5c217ca28c3
Blanca <66270936+blancalbg@users.noreply.github.com>
2023-03-07 12:36:43 +0100

    Update streamlit_app.py
    changed streamlit attributes that were wrong (header and captions)

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.subtitle('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.markdown('Breakfast Menu')
+streamlit.caption('Omega 3 & Blueberry Oatmeal')
+streamlit.caption('Kale, Spinach & Rocket Smoothie')
+streamlit.caption('Hard-Boiled Free-Range Egg')
