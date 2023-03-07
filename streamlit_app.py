38b8b77bb7fb584f66a2fbf96f2f87c89c586f90
Blanca <66270936+blancalbg@users.noreply.github.com>
2023-03-07 12:37:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.markdown('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.caption('Omega 3 & Blueberry Oatmeal')
 streamlit.caption('Kale, Spinach & Rocket Smoothie')
 streamlit.caption('Hard-Boiled Free-Range Egg')
